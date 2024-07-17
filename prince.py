cf49c4345e25ee681793f9f39e7cfb4b4f418fdd
loyal-guy <aguprincenonso@gmail.com>
2024-07-17 10:05:19 +0100

    prince class

---
+++ prince.py
@@ -0,0 +1,57 @@
+# print('hello word')
+
+# x, y, z = 10, 40, 50
+# print (z)
+
+# first_name = 'tunde'
+# last_name ='john'
+
+# # concatination is the process of joining two variables together
+# print(first_name, last_name)
+# print(first_name+ ' '+ last_name)
+# print(f'{first_name} {last_name}')
+      
+# # creation of programme
+# first_name = input("please enter your first name : ")
+# last_name = input("please enter your last name : ")
+# print(f'good morning Mr {first_name} {last_name}')
+
+# first_number = int(input("enter your first number : "))
+# second_number = int(input("enter your second number : "))
+
+# print(f'{first_number} + {second_number} = {first_number + second_number}')
+
+# num1 = str(10)
+# print(type(num1))
+
+# # data type
+# # string
+# list
+# dictionary
+# number
+# set
+# boolean
+# tuple
+
+# address = "47, herbert macauly way, off \"university\" road, akoka yaba lagos state"
+# print(address[0:10:2])
+# print(address.upper())
+# print(address.lower())
+# print(address.replace('lagos', 'ogun'))
+# print(address.startswith('4'))
+# print(address.endswith('e'))
+# print(address.isalnum())
+# print(address.index('4'))
+# print(address.find('h'))
+# x = address.split(' ')
+# x.reverse()
+# print(x)
+
+# first_name = input("please enter your first name : ")
+# second_name = input("please enter your last name : ")
+# third_name = input("please enter your age : ")
+# print(f'good morning Mr {first_name} {second_name} you are {third_name}')
+
+first_name = input("what is your favourite color : ")
+second_name = input("what is your favourite animal : ")
+print(f'good to know that {first_name} is your favourite color and that {second_name} is your favourite animal')
